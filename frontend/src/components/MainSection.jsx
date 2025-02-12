091e1bc741889855ba389b24d6f5935fc632f8bb
oreki <orekihuu@proton.me>
2025-02-12 09:11:33 +0530

    changed url

--- frontend/src/components/MainSection.jsx
+++ frontend/src/components/MainSection.jsx
@@ -12,7 +12,7 @@ function MainSection() {
   const fetchData = async () => {
     try {
       setLoading(true);
-      const response = await axios.get(`http://localhost:3000/walls`);
+      const response = await axios.get(`https://getwalls.osc-fr1.scalingo.io/walls`);
       setData(response.data);
     } catch (error) {
       console.error("Error fetching data:", error);
